d79ce7d5f03f3a7a5c8ea38d87b4dd6a3ffce2bc
Benjamin E. Valencia - Bencod <benyamin.ak@hotmail.com>
2017-05-25 13:57:10 +0200

    conecction to the db

---
+++ script/php/functions/db_connect.php
@@ -0,0 +1,32 @@
+<?php 
+$host = 'localhost';
+$user= 'root';
+$password = '';
+$db = 'project';
+?>
+
+
+<?php
+    abstract class PDORepository{
+        const USERNAME="root";
+        const PASSWORD="";
+        const HOST="localhost";
+        const DB="project";
+
+        private function getConnection(){
+            $username = self::USERNAME;
+            $password = self::PASSWORD;
+            $host = self::HOST;
+            $db = self::DB;
+            $connection = new PDO("mysql:dbname=$db;host=$host", $username, $password);
+            return $connection;
+        }
+        protected function queryList($sql, $args){
+            $connection = $this->getConnection();
+            $stmt = $connection->prepare($sql);
+            $stmt->execute($args);
+            return $stmt;
+        }
+    }
+
+?><
