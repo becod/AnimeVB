d8b967231212ae204050f6fc7f143ed3f986cd0c
Benjamin E. Valencia - Bencod <benyamin.ak@hotmail.com>
2017-06-12 00:14:45 +0200

    index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 if (isset($_GET['view'])){
-    if( file_exists('core/controllers/'.$_GET['view'].'Controller.php')){
-        include('core/controllers/'.$_GET['view'].'Controller.php');
+    if( file_exists('core/controllers/'.strtolower($_GET['view']).'Controller.php')){
+        include('core/controllers/'.strtolower($_GET['view']).'Controller.php');
     } else {
     echo 'error ';
     }
