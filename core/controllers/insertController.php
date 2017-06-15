e8c120da2a07ab06b1b3aa92bad1b4455d74ec93
Benjamin E. Valencia - Bencod <benyamin.ak@hotmail.com>
2017-06-15 23:50:56 +0200

    Completado por hoy, Falta el insert ,problemas al enviar el file a PHP

--- core/controllers/insertController.php
+++ core/controllers/insertController.php
@@ -1,20 +1,21 @@
 <?php
-    require_once ('db_connect.php');
+    include('../core.php');
+    require_once ('../model/db_connect.php');
 
  	session_start();
  
         
-        $title=$_POST["title"];
+        $title= $_POST["title"];
         $about= $_POST["plot"];
         $img= "content/img/".$_FILES["image"]["name"];
                       
-		$i = move_uploaded_file($_FILES["image"]["tmp_name"],"../../../content/img/".$_FILES["image"]["name"]);
+		$i = move_uploaded_file($_FILES["image"]["tmp_name"],"../../content/img/".$_FILES["image"]["name"]);
           
-        if ($i){
-            $query = new PDOconnect;
-            $query -> queryList('Insert into animeinfo(name,about,img,id_user) values (:title, :about, :img, :id_user)', array(':title' => $title, ':about' => $about, ':img' => $img, ':id_user' => $_SESSION['id'] ));
+         if ($i){
+            /*$query = new PDOconnect;
+            $query -> queryList('Insert into animeinfo(name,about,img,id_user) values (:title, :about, :img, :id_user)', array(':title' => $title, ':about' => $about, ':img' => $img, ':id_user' => $_SESSION['id'] ));*/
         }
         else{
-            print 'bad';
+            print $img;
         } 
-?>
+?> 
